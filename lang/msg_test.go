f0902f4fe1758b2b15fd9b5e23a8fc6836b16e7e
1 <1@1.com>
2022-06-11 13:21:39 +0800

    a

--- lang/msg_test.go
+++ lang/msg_test.go
@@ -1,11 +1,26 @@
 package lang
 
 import (
-	"fmt"
+	"log"
+	"reflect"
 	"testing"
 )
 
 func TestName(t *testing.T) {
-	ok := NewOk()
-	fmt.Println(ok)
+	newOk := NewOk()
+	var ok any = NewOk()
+
+	msg := ok.(*Msg)
+	log.Println(msg.Code)
+
+	switch ok.(type) {
+	case *Msg:
+		log.Println("..................")
+	}
+
+	if reflect.ValueOf(ok).Type() == reflect.ValueOf(newOk).Type() {
+		log.Println("ok", reflect.ValueOf(ok).Type())
+	} else {
+		log.Println("no")
+	}
 }
