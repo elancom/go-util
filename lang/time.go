a33ee1fb24f42c5da2a02adfe00670e71da9ba69
1 <1@1.com>
2022-06-14 20:45:20 +0800

    a

--- lang/time.go
+++ lang/time.go
@@ -11,6 +11,10 @@ const formatDate = "2006-01-02"
 const formatDateTime = "2006-01-02 15:04:05"
 const formatTime = "15:04:05"
 
+func FormatDateTimes(t time.Time) string {
+	return FormatDateTime(&t)
+}
+
 func FormatDateTime(t *time.Time) string {
 	if t == nil {
 		return ""
@@ -18,11 +22,19 @@ func FormatDateTime(t *time.Time) string {
 	return t.Format(formatDateTime)
 }
 
+func FormatDates(t time.Time) string {
+	return FormatDateTime(&t)
+}
+
 func FormatDate(t *time.Time) string {
 	if t == nil {
 		return ""
 	}
 	return t.Format(formatDate)
+}
+
+func FormatTimes(t time.Time) string {
+	return FormatTime(&t)
 }
 
 func FormatTime(t *time.Time) string {
