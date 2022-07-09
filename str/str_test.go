21abd2a192ac6843541be74d05f06e6bed3ffb74
1 <1@1.com>
2022-07-09 22:48:01 +0800

    a

--- str/str_test.go
+++ str/str_test.go
@@ -3,8 +3,17 @@ package str
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"testing"
 )
+
+func Test1(t *testing.T) {
+	s := "123,123,1     23   123  981、72，31312。3213"
+	compile, err := regexp.Compile("[,。，、|\n]|\\s+")
+	log.Println(err)
+	allString := compile.ReplaceAllString(s, " ")
+	log.Println(allString)
+}
 
 func TestName(t *testing.T) {
 	fmt.Println(String(123.12))
