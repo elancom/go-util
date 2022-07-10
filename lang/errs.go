11e590305ce291372c1eb266a3f6e1abafdc4094
1 <1@1.com>
2022-07-10 18:19:11 +0800

    a

--- lang/errs.go
+++ lang/errs.go
@@ -2,8 +2,5 @@ package lang
 
 import "errors"
 
-var NotFound = errors.New("not found")
-
-func isNotFound(err error) bool {
-	return err == NotFound
-}
+var NotFound = errors.New("NotFound")
+var NotAuthorized = errors.New("NotAuthorized")
